be36b7bdf5496f941f08c26dcf969be248896497
Dalvis <Dalvis.liz27@gmail.com>
2023-07-26 14:49:16 -0400

    finished exercise two

--- index.test.js
+++ index.test.js
@@ -17,8 +17,16 @@ describe('[Exercise 1] trimProperties', () => {
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
-  // test('[3] returns an object with the properties trimmed', () => {})
-  // test('[4] the object returned is the exact same one we passed in', () => {})
+  test('[3] returns an object with the properties trimmed', () => {
+    const object = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    utils.trimPropertiesMutation(object)
+    expect(object).toEqual({ foo: 'foo', bar: 'bar', baz: 'baz' })
+  })
+  test('[4] the object returned is the exact same one we passed in', () => {
+    const object = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const actual = utils.trimPropertiesMutation(object)
+    expect(actual).toBe(object)
+  })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
